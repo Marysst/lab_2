bb98a99091ef3cacad0bf7df79a7445d929e33cf
Marysst <161862691+Marysst@users.noreply.github.com>
2024-04-15 12:43:10 +0300

    Update test_another_implementation.py

--- test_another_implementation.py
+++ test_another_implementation.py
@@ -6,7 +6,7 @@ from another_implementation import encoder, function_correct, function_incorrect
 
 class TestAnotherImplementation(TestCase):
     @patch('sys.stdout', new_callable=StringIO)
-      def test_create_translation_table(self):
+    def test_create_translation_table(self):
         # Act
         translation_table = create_translation_table()
 
