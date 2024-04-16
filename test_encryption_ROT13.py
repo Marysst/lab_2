94e12b4e1b4a433495b7452b451b43a890e4400c
Marysst <161862691+Marysst@users.noreply.github.com>
2024-04-16 22:15:00 +0300

    Update test_encryption_ROT13.py

--- test_encryption_ROT13.py
+++ test_encryption_ROT13.py
@@ -1,8 +1,5 @@
 from unittest import TestCase
 from encryption_ROT13 import encoder, function_correct
-import subprocess
-
-subprocess.run(['icacls encryption_ROT13.py /grant Everyone:F'], shell=True)
 
 class TestEncoder(TestCase):
     def test_simple_string(self) -> None:
@@ -32,11 +29,10 @@ class TestEncoder(TestCase):
         input_string = 'hello фыЪ汉字'
         expected_output = 'String must contain only Latin alphabet, digits and special characters'
 
-        # Act
+        # Act & Assert
         with self.assertRaises(ValueError) as context:
             encoder(input_string)
         
-        # Assert
         self.assertEqual(expected_output, context.exception.__str__())
 
     def test_empty_string(self) -> None:
@@ -50,29 +46,8 @@ class TestEncoder(TestCase):
         # Assert
         self.assertEqual(expected_output, encoded_string)
 
-    def test_stdin_valid_input(self) -> None:
-        # Arrange
-        input_command = 'echo hello world'
-        expected_output = 'Encoded text is: uryyb jbeyq\n\n'
-        
-        # Act
-        result = subprocess.run([f'{input_command} | python3 encryption_ROT13.py'], shell=True, stdout=subprocess.PIPE)
-        
-        # Assert
-        self.assertEqual(expected_output, result.stdout.decode('utf-8'))
-
-    def test_stdin_invalid_input(self) -> None:
-        # Arrange
-        input_command = 'echo hello фыЪ汉字'
-        expected_output = 'String must contain only Latin alphabet, digits and special characters\n'
-        
-        # Act
-        result = subprocess.run([f'{input_command} | python3 encryption_ROT13.py'], shell=True, stderr=subprocess.PIPE)
-        
-        # Assert
-        self.assertTrue(result.stderr.decode('utf-8').startswith(expected_output))
-
-    def test_exit_code(self) -> None:
+    def test_function_correct_exit_code(self) -> None:
+        # Act & Assert
         with self.assertRaises(SystemExit) as cm:
             function_correct()
             self.assertEqual(cm.exception.code, 0)
